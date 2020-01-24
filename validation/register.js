8e6209c7e5010f22344ea19231dec22f4e90d832
tahreen zeba <tahreenzeba1@gmail.com>
2020-01-24 07:44:30 -0500

    login and register validation

---
+++ validation/register.js
@@ -0,0 +1,33 @@
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = function validateRegisterInput(data) {
+    let errors = {};
+
+    data.name = !isEmpty(data.name) ? data.name : "";
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+
+    //check name
+    if (Validator.isEmpty(data.name)) {
+        errors.name = "Name field is required";
+    }
+    // check email
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required";
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid";
+    }
+    // check password
+    if (Validator.isEmpty.password){
+        errors.password = "Password field is required";
+    }
+    if(!Validator.isLength(data.password,{min:6, max:30})){
+        errors.password = "Password must be at least 6 characters";
+    }
+    return{
+        errors,
+        isValid: isEmpty(errors)
+    };
+
+};
