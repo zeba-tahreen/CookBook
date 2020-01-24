73946979f87d04cf6bbb5cefd4d399db6ddca02d
tahreen zeba <tahreenzeba1@gmail.com>
2020-01-24 07:50:29 -0500

    app startup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const users = require('./routes/api/users');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// passport middleware
+app.use(passport.initialize());
+// passport config
+require('./config/passport')(passport);
+// routes
+app.use('/api/users', users);
+
+
+const db = require("./config/keys").mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("CookBook Database is connected"))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port} 🌎 !!`));
