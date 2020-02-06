0050eb1323d4358a041cb02fca5b20ad29ace96d
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-06 03:27:13 -0500

    navigate the routes by isAuthenticated -reactRedux

--- client/src/components/layouts/NavBar.js
+++ client/src/components/layouts/NavBar.js
@@ -1,49 +1,64 @@
 import React, { Component } from 'react';
-
-// react styled-components
+import PropTypes from "prop-types";
 import style from 'styled-components';
-// link router
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import { logoutUser } from "../../actions/authActions";
 
 class NavBar extends Component {
 
+    logout = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+    };
+
     render() {
+        const { isAuthenticated } = this.props.auth;
+
+        const userLinks = (
+            <ul>
+                <ListGroupItem >
+                    <a className="nav-item ml-5" href="/recipe">Recipe&nbsp;<i className="fas fa-book-open"></i></a>
+                </ListGroupItem>
+                <ListGroupItem >
+                    <a className="nav-item ml-5" href="/kitchen">Kitchen&nbsp;<i className="fas fa-concierge-bell"></i></a>
+                </ListGroupItem>
+                <ListGroupItem >
+                    <a className="nav-item ml-5" href="/logout" onClick={this.logout.bind(this)}>Logout</a>
+                </ListGroupItem>
+            </ul>
+        );
+
+        const guestLinks = (
+            <ul>
+                <ListGroupItem >
+                    <a className="nav-item ml-5" href="/recipe">Recipe&nbsp;<i className="fas fa-book-open"></i></a>
+                </ListGroupItem>
+                <ListGroupItem >
+                    <a className="nav-item ml-5" href="/login">Login</a>
+                </ListGroupItem>
+                <ListGroupItem >
+                    <a className="nav-item ml-5" href="/register">Register</a>
+                </ListGroupItem>
+            </ul>
+
+        );
         return (
-            <NavbarContainer className = "nav-header">
+            <NavbarContainer className="nav-header">
                 <nav className="navbar navbar-expand-lg navbar-light ">
                     <Link className="navbar-brand  text-white" to="/">CookBook</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
-                        <span> <i className= "fas fa-bars" style={{color:"#fffff"}}></i> </span>
+                        <span> <i className="fas fa-bars" style={{ color: "#fffff" }}></i> </span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <form className="form-inline ml-5 ">
+                        <form className="form-inline ml-5 ">
                             <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-                            {/* <button className="btn btn-outline my-2 my-sm-0" type="submit">Search</button> */}
                         </form>
                         <ul className="navbar-nav m-auto">
-                            <li className="nav-item active">
-                                <Link className="nav-link text-white ml-5 " to="/recipe">Recipe&nbsp;<i className="fas fa-book-open"></i> <span className="sr-only">(current)</span></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link text-white ml-5 " to="/kitchen"> Kitchen&nbsp;<i className="fas fa-concierge-bell"></i> </Link>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <Link className="nav-link dropdown-toggle  text-white ml-5" to="#" id="navbarDropdown" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Register/Login</Link>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <Link className="dropdown-item" to="/login">Login</Link>
-                                    <Link className="dropdown-item" to="/register">Register</Link>
-                                    <Link className="dropdown-item" to="/contact">Contact us</Link>
-                                    <div className="dropdown-divider"></div>
-                                    <Link className="dropdown-item" to="/">logout</Link>
-                                </div>
-                            </li>
-
+                            {isAuthenticated ? userLinks : guestLinks}
                         </ul>
 
                     </div>
@@ -53,8 +68,18 @@ class NavBar extends Component {
         )
     }
 }
+NavBar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
 
-export default NavBar;
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+export default connect(
+    mapStateToProps,
+    { logoutUser }
+)(NavBar);
 
 const NavbarContainer = style.nav`
 .navbar{
@@ -76,8 +101,15 @@ const NavbarContainer = style.nav`
 .navbar-toggler {
     border:none;
 }
-.dropdown-item:hover {
-background: rgb(199, 164, 8);
-color: white;
-}
+
+`
+
+const ListGroupItem = style.div`
+   background:none;
+   border:none;
+   display:inline;
+
+   .nav-item{
+       color:white;
+   }
 `
